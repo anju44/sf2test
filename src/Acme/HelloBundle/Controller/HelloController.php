8c74dd9c2e064d8a9ba171ba8b2d77eda9dca073
Julie Meunier <ju.meunier44@gmail.com>
2015-01-08 11:15:42 +0100

    Le contrôleur

--- src/Acme/HelloBundle/Controller/HelloController.php
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -1,12 +1,28 @@
 <?php
 	namespace Acme\HelloBundle\Controller;
 
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 	use Symfony\Component\HttpFoundation\Response;
 
-	class HelloController
+	class HelloController extends Controller
 	{
-		public function indexAction($name)
+		public function indexAction($first_name, $last_name, $color)
 		{
-			return new Response('<html><body>Hello '.$name.'!</body></html>');
+			// return new Response('<html><body>Hello '. sprintf("%s %s", $first_name, $last_name) .'!</body></html>');
+			//return $this->redirect($this->generateUrl('homepage'), 301);
+
+			return $this->render(
+    			'AcmeHelloBundle:Hello:index.html.twig',
+    			array('first_name' => $first_name,
+				'last_name' => $last_name,
+				'color' => $color));		
+
+			// récupérer l'objet depuis la base de données
+			$product = 'truc';
+			if (!$product)
+			{
+				thorw $this->createNotFoundException('Le produit n\'existe pas');
+			}
+			return $this->render('truc');
 		}
 	}
