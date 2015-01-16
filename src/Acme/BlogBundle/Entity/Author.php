a21d103c31b7df984914723f49105f2aed2f41bc
Julie Meunier <ju.meunier44@gmail.com>
2015-01-16 12:51:39 +0100

    Chapitre Traduction

---
+++ src/Acme/BlogBundle/Entity/Author.php
@@ -0,0 +1,8 @@
+<?php
+
+	namespace Acme\BlogBundle\Entity
+
+	class Author
+	{
+		public $name;
+	}
--- src/Acme/HelloBundle/Controller/HelloController.php
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -8,13 +8,18 @@
 	{
 		public function indexAction($first_name, $last_name, $color)
 		{
-			// return new Response('<html><body>Hello '. sprintf("%s %s", $first_name, $last_name) .'!</body></html>');
+			//return new Response('<html><body>Hello '. sprintf("%s %s", $first_name, $last_name) .'!</body></html>');
 			//return $this->redirect($this->generateUrl('homepage'), 301);
 
 			return $this->render(
-    			'AcmeHelloBundle:Hello:index.html.twig',
-    			array('first_name' => $first_name,
+   			'AcmeHelloBundle:Hello:index.html.twig',
+   			array('first_name' => $first_name,
 				'last_name' => $last_name,
 				'color' => $color));		
+
+			// Chapitre traduction
+			//$t = $this->get('translator')->trans('Symfony is great');
+			//return new Response($t);
 		}
+
 	}
